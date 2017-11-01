2105fd57c51a15e79b41b3f585c5f9ebe54a3021
Erik Myers <erikmyers@eriks-mbp.attlocal.net>
2017-11-01 11:44:04 -0700

    removed unused import

--- src/my-command.js
+++ src/my-command.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { render, Artboard } from 'react-sketchapp';
 import SwatchList from './components/SwatchList';
 import { colorPalettes } from './colors';
